7cd8666ae8c00f66f4b29c8fdb99e35c0c6e638b
Will Chen <108688228+will-d-chen@users.noreply.github.com>
2024-04-22 20:37:35 -0400

    Update util.py

--- util.py
+++ util.py
@@ -2,16 +2,13 @@ import google.generativeai as genai
 from PIL import Image
 
 # Local
-from constants import RECYCLING_INSTRUCTIONS_PROMPT, RECYCLING_LOCATIONS_PROMPT_COORDINATES, RECYCLING_LOCATIONS_PROMPT_DURHAM, STREAMLIT_CLOUD_HOSTNAME
-
-
-#__current_system_hostname__ = socket.gethostname()
+from constants import INSTRUCTIONS_PROMPT, 
 
 
 # Function to call the Gemini LLM API (you'll need to replace with actual API details)
-def get_recycling_instructions(image: Image, gemini_model: genai.GenerativeModel) -> str:
+def get_instructions(image: Image, gemini_model: genai.GenerativeModel) -> str:
     response = gemini_model.generate_content([
-        RECYCLING_INSTRUCTIONS_PROMPT,
+        INSTRUCTIONS_PROMPT,
         image,
     ])
     response.resolve()
